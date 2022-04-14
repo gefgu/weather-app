401ba98fa117c92fe71f9bebdd79e359c3e853d8
gefgu <gefgu@hotmail.com>
2022-04-14 10:36:02 -0300

    Add processing of data

--- script.js
+++ script.js
@@ -4,10 +4,16 @@
     const requestLink = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiID}`;
     const response = await fetch(requestLink);
     const data = await response.json();
+    console.log(data);
     return data;
   }
 
+  function processData(rawData) {
+    const weatherData = rawData.main;
+    return weatherData;
+  }
+
   getData("London").then((response) => {
-    console.log(response);
+    console.log(processData(response));
   });
 })();
