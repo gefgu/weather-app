367f630eb5d190195f4a252284fa6332ae158b2e
gefgu <gefgu@hotmail.com>
2022-04-14 10:31:01 -0300

    Add call to open weather api

---
+++ script.js
@@ -0,0 +1,10 @@
+(() => {
+  async function getData(location) {
+    const apiID = "9426f14f9c66ae5af9513a6760935f0f";
+    const requestLink = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiID}`;
+    const response = await fetch(requestLink);
+    console.log(response);
+  }
+
+  getData("London");
+})();
