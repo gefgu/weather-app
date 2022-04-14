0fa7cd812712340e09c49f597a933343214e8ecd
gefgu <gefgu@hotmail.com>
2022-04-14 10:38:55 -0300

    Add new nameming for clarity

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 (() => {
-  async function getData(location) {
+  async function getRawData(location) {
     const apiID = "9426f14f9c66ae5af9513a6760935f0f";
     const requestLink = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiID}`;
     const response = await fetch(requestLink);
     const data = await response.json();
-    console.log(data);
     return data;
   }
 
@@ -13,7 +12,7 @@
     return weatherData;
   }
 
-  getData("London").then((response) => {
+  getRawData("London").then((response) => {
     console.log(processData(response));
   });
 })();
