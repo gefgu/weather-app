19b3fd248eb45ddcf38c8d676645ee02ab5e8c0b
gefgu <gefgu@hotmail.com>
2022-04-14 10:33:52 -0300

    Add get json of data and promise handling to console the data

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
     const apiID = "9426f14f9c66ae5af9513a6760935f0f";
     const requestLink = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiID}`;
     const response = await fetch(requestLink);
-    console.log(response);
+    const data = await response.json();
+    return data;
   }
 
-  getData("London");
+  getData("London").then((response) => {
+    console.log(response);
+  });
 })();
